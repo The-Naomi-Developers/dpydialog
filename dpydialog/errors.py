1614f781fa21f2f88c42203e0cac10cf2cb2f9aa
Starrysparklez <starrysparklez@naomi.win>
2025-04-16 13:42:29 +0600

    Added the `allowed_ids` and `user_id` arguments to the "NotAllowedToInteract" exception class.

--- dpydialog/errors.py
+++ dpydialog/errors.py
@@ -1,9 +1,9 @@
-from typing import Optional
+from typing import Optional, Sequence
 
 
 class DialogException(Exception):
     """The base class of the DPyDialogue library."""
-    def __init__(self, *args, stage_keyname: str = None):
+    def __init__(self, *args, stage_keyname: Optional[str] = None):
         super().__init__(*args)
         self._stage_keyname: Optional[str] = stage_keyname
 
@@ -18,6 +18,16 @@ class ShouldBeCoroutine(DialogException):
 
 class NotAllowedToInteract(DialogException):
     """Raised when a current user is not allowed to interact with the component."""
+    def __init__(self, *args, allowed_ids: Sequence[int], user_id: int, stage_keyname: Optional[str] = None):
+        self._stage_keyname = stage_keyname
+        self._allowed_ids = allowed_ids
+        self._uid = user_id
+    
+    def get_allowed_users(self) -> Sequence[int]:
+        return self._allowed_ids
+
+    def get_user_id(self) -> int:
+        return self.uid
 
 class DialogHasNoStages(DialogException):
     """Raised when a `Dialog` is sent without `Stage` classes."""
